902f8c0c1e772041ec61d3fb5fb39132fa668b17
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-02-16 16:52:23 -0500

    Erie datapull script

--- lake_erie_datapull.py
+++ lake_erie_datapull.py
@@ -1,31 +1,11 @@
 import read_into_xarray
 from read_into_xarray import DataAccessor
 from pathlib import Path
-"""
-Hi Xavier,
+import gc
 
-Per our discussion a bit ago, we would like you to extract the climate model output at each of our model cell locations.  As I mentioned, some of the variables we need are assigned to “elements” (triangular cells) and some are assigned to “nodes” (vertices of the triangular cells).  Both of those model shapefiles are located in the paths below:
-
-Wind components are assigned at “elements”:       
-I:\LEEM\GIS\data\FVCOM\Grids\Fine\FVCOM_Elements.shp
-Air temp, cloud cover, dew point temp and shortwave radiation are assigned at “nodes”:  
-I:\LEEM\GIS\data\FVCOM\Grids\Fine\FVCOM_Nodes.shp
-Lake Erie boundary shapefile:  I:\LEEM\GIS\data\FVCOM\Grids\Fine\LEEM_boundary.shp
-
-Also, the attached excel file contains the lat and lon for all of the locations for both shapefiles.  
-
-For the spatial resampling/interpolation, it looks like the smallest triangular cells are on the order of 0.1 km2, so we don’t need to get super fine there.  Use your best judgement on what is feasible for computer resources.
-
-Hourly output would be ideal.  And ultimately, if we find this climate data to be useful, we would need 2011-2021, but starting with a shorter time period is fine.  Lets say 9/1/2014-10/1/2016.  If that’s too much data, then try 9/1/2015-10/1/2016?
-
-Thanks a bunch
-
--Dan
-"""
-START_TIME = '9/1/2015'
-END_TIME = '9/30/2016'
-AOI_SHP = Path(Path.cwd() / 'lake_erie_data/LEEM_boundary.shp')
-RESOLUTION = 100 # meters
+LAKE_ERIE_DIR = Path(Path.cwd() / 'lake_erie_data/')
+AOI_SHP = Path(LAKE_ERIE_DIR / 'LEEM_boundary.shp')
+RESOLUTION = 100  # meters
 DATASET_NAME = 'reanalysis-era5-single-levels'
 VARIABLES = [
     '2m_temperature',
@@ -36,16 +16,70 @@ VARIABLES = [
     '10m_v_component_of_wind',
 ]
 
-data_accessor = DataAccessor(
-    dataset_name=DATASET_NAME,
-    variables=VARIABLES,
-    start_time=START_TIME,
-    end_time=END_TIME,
-    coordinates=None,
-    csv_of_coords=None,
-    shapefile=AOI_SHP,
-    raster=None,
-    multithread=True,
-)
+# set up directories for elements and nodes
+NODES_DIR = Path(LAKE_ERIE_DIR / 'nodes_data')
+if not NODES_DIR.exists():
+    NODES_DIR.mkdir()
+ELEMENTS_DIR = Path(LAKE_ERIE_DIR / 'nodes_data')
+if not ELEMENTS_DIR.exists():
+    ELEMENTS_DIR.mkdir()
 
-data_accessor.get_data()
+NODES_VARS = [
+    '2m_temperature',
+    '2m_dewpoint_temperature',
+    'total_cloud_cover',
+    'mean_surface_downward_short_wave_radiation_flux',
+]
+ELEMENT_VARS = [
+    '10m_u_component_of_wind',
+    '10m_v_component_of_wind',
+]
+
+# get data for a year at a time to avoid killing our memory
+for year in range(2011, 2022):
+    start_time = f'9/1/{year}'
+    end_time = f'9/30/{year + 1}'
+
+    # init our data accessor
+    data_accessor = DataAccessor(
+        dataset_name=DATASET_NAME,
+        variables=VARIABLES,
+        start_time=start_time,
+        end_time=end_time,
+        coordinates=None,
+        csv_of_coords=None,
+        shapefile=AOI_SHP,
+        raster=None,
+        multithread=True,
+    )
+
+    # get the data
+    data_accessor.get_data(
+        resolution_factor=30,
+        chunk_dict={'time': 5},
+    )
+
+    # convert data_to_table for NODES
+    out_dir = Path(NODES_DIR / f'water_year_{year}to{year + 1}_data')
+    if not out_dir.exists():
+        out_dir.mkdir()
+    data_accessor.get_data_tables(
+        variables=NODES_VARS,
+        save_table_dir=out_dir
+    )
+
+    # convert data_to_table for ELEMENTS
+    out_dir = Path(ELEMENTS_DIR / f'water_year_{year}to{year + 1}_data')
+    if not out_dir.exists():
+        out_dir.mkdir()
+    data_accessor.get_data_tables(
+        variables=ELEMENT_VARS,
+        save_table_dir=out_dir
+    )
+
+    # remove temp files
+    data_accessor.unlock_and_clean()
+
+    # delete the data accessor and clear memory
+    del data_accessor
+    gc.collect()
