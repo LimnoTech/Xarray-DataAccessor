43c1640881936f746928be4875e65c4029a2eeb8
modelrun <100317369+modelrun@users.noreply.github.com>
2023-02-17 00:38:27 -0500

    Update lake_erie_datapull.py

--- lake_erie_datapull.py
+++ lake_erie_datapull.py
@@ -20,7 +20,7 @@ VARIABLES = [
 NODES_DIR = Path(LAKE_ERIE_DIR / 'nodes_data')
 if not NODES_DIR.exists():
     NODES_DIR.mkdir()
-ELEMENTS_DIR = Path(LAKE_ERIE_DIR / 'nodes_data')
+ELEMENTS_DIR = Path(LAKE_ERIE_DIR / 'elements_data')
 if not ELEMENTS_DIR.exists():
     ELEMENTS_DIR.mkdir()
 
@@ -35,51 +35,53 @@ ELEMENT_VARS = [
     '10m_v_component_of_wind',
 ]
 
+COL_IDS = ['NELE', 'NODE']
+CSVS = [
+    Path(LAKE_ERIE_DIR / 'element_latlongs.csv'),
+    Path(LAKE_ERIE_DIR / 'node_latlongs.csv'),
+]
+OUT_DIRS = [ELEMENTS_DIR, NODES_DIR]
+MONTH_CHUNKS = ['jan_thru_june', 'july_thru_dec']
+
 # get data for a year at a time to avoid killing our memory
-for year in range(2011, 2022):
-    start_time = f'9/1/{year}'
-    end_time = f'9/30/{year + 1}'
+for i, var_list in enumerate([ELEMENT_VARS, NODES_VARS]):
+    for year in range(2011, 2022):
+        for j, months_chunk in enumerate([('1/01', '6/30'), ('7/01', '12/31')]):
+            start_time = f'{months_chunk[0]}/{year}'
+            end_time = f'{months_chunk[1]}/{year}'
 
-    # init our data accessor
-    data_accessor = DataAccessor(
-        dataset_name=DATASET_NAME,
-        variables=VARIABLES,
-        start_time=start_time,
-        end_time=end_time,
-        coordinates=None,
-        csv_of_coords=None,
-        shapefile=AOI_SHP,
-        raster=None,
-        multithread=True,
-    )
+            # init our data accessor
+            data_accessor = DataAccessor(
+                dataset_name=DATASET_NAME,
+                variables=var_list,
+                start_time=start_time,
+                end_time=end_time,
+                coordinates=None,
+                csv_of_coords=None,
+                shapefile=AOI_SHP,
+                raster=None,
+                multithread=True,
+            )
 
-    # get the data
-    data_accessor.get_data(
-        resolution_factor=30,
-        chunk_dict={'time': 5},
-    )
+            # get the data
+            data_accessor.get_data(
+                resolution_factor=20,
+                chunk_dict={'time': 5},
+            )
 
-    # convert data_to_table for NODES
-    out_dir = Path(NODES_DIR / f'water_year_{year}to{year + 1}_data')
-    if not out_dir.exists():
-        out_dir.mkdir()
-    data_accessor.get_data_tables(
-        variables=NODES_VARS,
-        save_table_dir=out_dir
-    )
+            # convert data_to_table for NODES
+            prefix = f'{MONTH_CHUNKS[j]}_{year}_'
+            data_accessor.get_data_tables(
+                variables=var_list,
+                csv_of_coords=CSVS[i],
+                coords_id_column=COL_IDS[i],
+                save_table_dir=OUT_DIRS[i],
+                save_table_prefix=prefix,
+            )
 
-    # convert data_to_table for ELEMENTS
-    out_dir = Path(ELEMENTS_DIR / f'water_year_{year}to{year + 1}_data')
-    if not out_dir.exists():
-        out_dir.mkdir()
-    data_accessor.get_data_tables(
-        variables=ELEMENT_VARS,
-        save_table_dir=out_dir
-    )
+            # remove temp files
+            data_accessor.unlock_and_clean()
 
-    # remove temp files
-    data_accessor.unlock_and_clean()
-
-    # delete the data accessor and clear memory
-    del data_accessor
-    gc.collect()
+            # delete the data accessor and clear memory
+            del data_accessor
+            gc.collect()
