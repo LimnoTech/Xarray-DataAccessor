59a058ae3c8fe0e67ee35a316650220679c2e324
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-04-14 17:35:10 -0400

    Create nasa_from_PODAAC_info.py

---
+++ src/xarray_data_accessor/data_accessors/nasa_from_PODAAC_info.py
@@ -0,0 +1,9 @@
+"""
+Provider: PO DAAC = Physical Oceanography Distributed Active Archive Center.
+Info: PO DAAC is a NASA mission. It is a component of the
+    Earth Observing System Data and Information System (EOSDIS).
+Data Source: Data is pulled from the PO DAAC Cloud hosted OPeNDAP server.
+    See: https://opendap.jpl.nasa.gov/opendap/
+Data Access: One must have an Earthdata Login account to access the data.
+    Sign up here: https://urs.earthdata.nasa.gov/users/new
+"""
