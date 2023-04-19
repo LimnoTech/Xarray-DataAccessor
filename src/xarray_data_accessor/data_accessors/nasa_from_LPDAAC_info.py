234051e6231456ab4ef28fdf22d067d35d3b7003
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-04-19 11:12:07 -0400

    consolidate nasa_info

--- src/xarray_data_accessor/data_accessors/nasa_from_LPDAAC_info.py
+++
@@ -1,9 +0,0 @@
-"""
-Provider: LP DAAC = Land Processes Distributed Active Archive Center.
-Info: LP DAAC is a partnership between USGS and NASA. It is a component of the 
-    Earth Observing System Data and Information System (EOSDIS).
-Data Source: Data is pulled from the LP DAAC Data Pool.
-    See: https://lpdaac.usgs.gov/tools/data-pool/
-Data Access: One must have an Earthdata Login account to access the data.
-    Sign up here: https://urs.earthdata.nasa.gov/users/new
-"""
