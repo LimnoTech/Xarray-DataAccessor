afbb47bd4315a2e00dc6f7d7f87e2891e0d93b2d
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-05-31 13:36:09 -0400

    Update __init__.py

--- src/xarray_data_accessor/__init__.py
+++ src/xarray_data_accessor/__init__.py
@@ -4,7 +4,7 @@ This package provides a set of functions for pulling in, and working with
 gridded environmental data.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.0.1'
 
 from xarray_data_accessor.core_functions import (
     get_xarray_dataset,
