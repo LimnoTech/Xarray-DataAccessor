908c1e87721653792d9ed0915b7ae98bba3cdb46
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2023-06-12 13:54:16 -0400

    Update __init__.py

--- src/xarray_data_accessor/info/__init__.py
+++ src/xarray_data_accessor/info/__init__.py
@@ -1,2 +1,4 @@
-"""Init file for info subpackage. Contain information for other functions.
-"""
+"""Init file for info subpackage. Contain information for other functions."""
+import xarray_data_accessor.info.era5 as era5
+import xarray_data_accessor.info.nasa as nasa
+import xarray_data_accessor.info.gssha as gssha
